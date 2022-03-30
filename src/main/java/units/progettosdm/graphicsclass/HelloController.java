e2802c79aef7e73326cee1227f21e1d295abecdb
MatteoBille <bille.matteo@gmail.com>
2022-03-30 22:34:04 +0200

    Inizializza e seleziona arco

---
+++ src/main/java/units/progettosdm/graphicsclass/HelloController.java
@@ -0,0 +1,14 @@
+package units.progettosdm.graphicsclass;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
