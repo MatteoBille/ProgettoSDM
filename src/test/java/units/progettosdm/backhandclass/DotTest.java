33e7b23347d623e23aaec9ff79800510847b47b6
MatteoBille <bille.matteo@gmail.com>
2022-03-31 12:01:16 +0200

    refactor some tests

--- src/test/java/units/progettosdm/backhandclass/DotTest.java
+++ src/test/java/units/progettosdm/backhandclass/DotTest.java
@@ -7,20 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class DotTest {
 
     @Test
-    void dotInizializationIsCorrect(){
-        Dot dot = new Dot(2,3);
-        assertArrayEquals(new int[]{2,3},dot.getDotIndex());
+    void dotInizializationIsCorrect() {
+        Dot dot = new Dot(2, 3);
+        assertArrayEquals(new int[]{2, 3}, dot.getDotIndex());
     }
 
     @Test
-    void dotIsEqualWorkProperly(){
-        Dot dot1 = new Dot(2,3);
-        Dot dot2 = new Dot(2,3);
-        Dot dot3 = new Dot(3,3);
+    void dotIsEqualWorkProperly() {
+        Dot dot1 = new Dot(2, 3);
+        Dot dot2 = new Dot(2, 3);
+        Dot dot3 = new Dot(3, 3);
 
-        assertTrue(dot1.equals(dot2));
-        assertTrue(dot2.equals(dot1));
-        assertFalse(dot2.equals(dot3));
-        assertFalse(dot2.equals(null));
+        assertEquals(dot1, dot2);
+        assertEquals(dot2, dot1);
+        assertNotEquals(dot2, dot3);
     }
 }
