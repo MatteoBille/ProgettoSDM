7ac306247a22eff4ac080765d8e6ca8014d19550
MatteoBille <bille.matteo@gmail.com>
2022-04-25 10:07:06 +0200

    StartPageController refactor

--- src/main/java/units/progettosdm/graphicsclass/StartPageController.java
+++ src/main/java/units/progettosdm/graphicsclass/StartPageController.java
@@ -15,37 +15,36 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Collections;
 
-import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
-
 
 public class StartPageController {
     @FXML
-    Pane parentPane;
+    Pane mainPane;
+
     @FXML
     TextField namePlayer1;
     @FXML
+    Label errorPlayer1;
+
+    @FXML
     TextField namePlayer2;
     @FXML
-    TextField nDimension;
-    @FXML
-    TextField mDimension;
-    @FXML
-    Label errorPlayer1;
-    @FXML
     Label errorPlayer2;
+
     @FXML
-    private Stage stage;
+    TextField numberOfColumn;
+    @FXML
+    TextField numberOfRows;
+
 
     @FXML
     protected void onStartGameButtonClick(ActionEvent event) throws IOException {
         if (validateTextField()) {
             FXMLLoader fxmlLoader = new FXMLLoader(StartPageController.class.getResource("gamePage.fxml"));
             int n, m;
-            n = Integer.parseInt(nDimension.getText());
-            m = Integer.parseInt(mDimension.getText());
+            n = Integer.parseInt(numberOfColumn.getText());
+            m = Integer.parseInt(numberOfRows.getText());
 
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(fxmlLoader.load());
             scene.getStylesheets().add(StartPageController.class.getResource("styleGamePage.css") + "");
             stage.setScene(scene);
@@ -61,36 +60,46 @@ public class StartPageController {
 
     private boolean validateTextField() {
         boolean response = true;
-        if (namePlayer1.getStyleClass().contains("error")) {
-            namePlayer1.getStyleClass().removeAll(Collections.singleton("error"));
-            errorPlayer1.setText("");
+        if (containErrorStyle(namePlayer1)) {
+            removeErrorStyleAndSetErrorLabelEmpty(namePlayer1, errorPlayer1);
         }
-        if (namePlayer2.getStyleClass().contains("error")) {
-            namePlayer2.getStyleClass().removeAll(Collections.singleton("error"));
-            errorPlayer2.setText("");
+        if (containErrorStyle(namePlayer2)) {
+            removeErrorStyleAndSetErrorLabelEmpty(namePlayer2, errorPlayer2);
         }
 
-        if (namePlayer1.getText().equals("")) {
-            response = noNameError(namePlayer1, errorPlayer1);
+        if (textInTextFieldIsEqual(namePlayer1, "")) {
+            setErrorStyleAndErrorText(namePlayer1, errorPlayer1,"Inserisci un nome");
+            response = false;
         }
-        if (namePlayer2.getText().equals("")) {
-            response = noNameError(namePlayer2, errorPlayer2);
+        if (textInTextFieldIsEqual(namePlayer2, "")) {
+            setErrorStyleAndErrorText(namePlayer2, errorPlayer2,"Inserisci un nome");
+            response = false;
         }
-        if (namePlayer1.getText().equals(namePlayer2.getText())) {
-            namePlayer1.getStyleClass().add("error");
-            namePlayer2.getStyleClass().add("error");
-            errorPlayer1.setText("Inserisci un nome diverso dall'altro giocatore");
-            errorPlayer2.setText("Inserisci un nome diverso dall'altro giocatore");
+        if (textInTextFieldIsEqual(namePlayer1, namePlayer2.getText())) {
+            setErrorStyleAndErrorText(namePlayer1, errorPlayer1,"Inserisci un nome diverso dall'altro giocatore");
+            setErrorStyleAndErrorText(namePlayer2, errorPlayer2,"Inserisci un nome diverso dall'altro giocatore");
 
             response = false;
         }
         return response;
     }
 
-    private boolean noNameError(TextField namePlayer2, Label errorPlayer2) {
+    private boolean textInTextFieldIsEqual(TextField namePlayer1, String s) {
+        return namePlayer1.getText().equals(s);
+    }
+
+    private void removeErrorStyleAndSetErrorLabelEmpty(TextField namePlayer1, Label errorPlayer1) {
+        namePlayer1.getStyleClass().removeAll(Collections.singleton("error"));
+        errorPlayer1.setText("");
+    }
+
+    private boolean containErrorStyle(TextField namePlayer1) {
+        return namePlayer1.getStyleClass().contains("error");
+    }
+
+    private void setErrorStyleAndErrorText(TextField namePlayer2, Label errorPlayer2,String text) {
         namePlayer2.getStyleClass().add("error");
-        errorPlayer2.setText("Inserisci un nome");
-        return false;
+        errorPlayer2.setText(text);
     }
 
 
@@ -103,7 +112,7 @@ public class StartPageController {
                 BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         Background bGround = new Background(bImg);
-        parentPane.setBackground(bGround);
+        mainPane.setBackground(bGround);
     }
 }
 
