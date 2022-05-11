5c2f1e03a32f18c2308c588a45bc11d3d2558966
rickybibi <riccardosaltarel@gmail.com>
2022-05-11 10:57:24 +0200

    ProperAssert in checkVictory

--- src/test/java/units/progettosdm/backendclass/GameTest.java
+++ src/test/java/units/progettosdm/backendclass/GameTest.java
@@ -33,6 +33,7 @@ public class GameTest {
         Game game = new Game(gridSize,gridSize, playerName1, playerName2);
         game.setScorePlayer1(20);
         game.setScorePlayer2(60);
+
         assertEquals("Giovanni", game.checkVictory());
     }
 }
